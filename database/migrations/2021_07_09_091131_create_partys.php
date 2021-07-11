d1bb8776700b42713d724386dcdfb4dcdce20f30
Pedro <Pedro.agullo.marco@gmail.com>
2021-07-11 23:44:41 +0200

    Kick off

---
+++ database/migrations/2021_07_09_091131_create_partys.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partys', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            // $table->foreignId('membership_id')->references('id')->on('membership');
+            $table->foreignId('game_id')->references('id')->on('games');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partys');
+    }
+}
