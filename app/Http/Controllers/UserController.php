3c12b1e69d0c09e7eb27bf3ccbc029347d54e33b
Pedro <Pedro.agullo.marco@gmail.com>
2021-07-14 00:08:54 +0200

    Added users endpoint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,9 +34,21 @@ class UserController extends Controller
      * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function show(User $user)
+    public function show($id)
     {
-        //
+        $user = auth()->user()->find($id);
+
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found '
+            ], 400);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $user->toArray()
+        ], 400);
     }
 
     /**
@@ -46,9 +58,29 @@ class UserController extends Controller
      * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, User $user)
+    public function update(Request $request)
     {
         //
+        $usuario = auth()->user()->find($request->$id);
+
+        if (!$usuario) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found'
+            ], 400);
+        }
+
+        $updated = $usuario->fill($request->all())->save();
+        
+        if ($updated)
+            return response()->json([
+                'success' => true
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'User can not be updated'
+            ], 500);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PassportAuthController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +20,7 @@ use App\Http\Controllers\PassportAuthController;
 Route::post('register', [PassportAuthController::class, 'register']);
 Route::post('login', [PassportAuthController::class, 'login']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::post('users', [UserController::class, 'update']);
+    Route::resource('users', UserController::class);
 });
