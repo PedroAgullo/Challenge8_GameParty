3434c20dcedf7d4c7465ef421f2788ed0d73adb2
pedroAGullo <pedro.agullo.marco@gmail.com>
2021-07-18 00:26:08 +0200

    Added search party by game

--- app/Http/Controllers/PartyController.php
+++ app/Http/Controllers/PartyController.php
@@ -65,9 +65,22 @@ class PartyController extends Controller
      * @param  \App\Models\Party  $party
      * @return \Illuminate\Http\Response
      */
-    public function show(Party $party)
+    public function show($game_id)
     {
         //
+
+        $resultado = Party::where('game_id', '=', $game_id)->get();
+        
+        if (!$resultado) {
+            return response() ->json([
+                'success' => false,
+                'data' => 'No se han encontrado partys con ese juego.'], 400);
+        } else {
+            return response() ->json([
+                'success' => true,
+                'data' => $resultado,
+            ], 200);
+        }
     }
 
     /**
@@ -88,8 +101,32 @@ class PartyController extends Controller
      * @param  \App\Models\Party  $party
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Party $party)
+    public function destroy($id)
     {
         //
+
+        $user = auth()-> user();
+
+        $party = Party::all()->find($id);
+
+        if ($user->isAdmin == true || $user->id == $party->userOwner){
+            
+            if ($party -> delete()){
+                return response()->json([
+                    'succes' => true,
+                    'message' => 'The party ' . $party->title . ' with the id ' . $party->id . ' has been deleted'
+                ]);
+            }else {
+                return response() ->json([
+                    'success' => false,
+                    'message' => 'Party can not be deleted',
+                ], 500);
+            }
+        }else{
+            return response() ->json([
+                'success' => false,
+                'message' => 'No tienes permiso para realizar esta acción.' . $party->userOwner . $user->id
+            ], 400);
+        }
     }
 }
